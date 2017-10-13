01fdda4452e06ba17c56042106fd0a66858f28e6
cassaram09 <cassaram09@gmail.com>
2017-10-12 22:56:12 -0400

    clean up archive

--- wp-content/themes/master/archive.php
+++ wp-content/themes/master/archive.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
 	<main role="main">
-		<!-- section -->
-		<section>
+
+		<section class='archive'>
 
 			<h1><?php _e( 'Archives', 'html5blank' ); ?></h1>
 
@@ -11,7 +11,7 @@
 			<?php get_template_part('pagination'); ?>
 
 		</section>
-		<!-- /section -->
+
 	</main>
 
 <?php get_sidebar(); ?>
