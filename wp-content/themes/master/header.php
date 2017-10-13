a3bdf3957afcbb4c246c4f29d01e5aae97ddb506
cassaram09 <cassaram09@gmail.com>
2017-10-12 21:56:40 -0400

    editing projects styles

--- wp-content/themes/master/header.php
+++ wp-content/themes/master/header.php
@@ -8,8 +8,6 @@
 
     <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
 
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
-
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
--- wp-content/themes/master/template-projects.php
+++ wp-content/themes/master/template-projects.php
@@ -1,15 +1,15 @@
 <?php /* Template Name: Projects Template */ get_header(); ?>
 
-  <main role="main" class='main-col'>
+  <main role="main" class='main'>
     <!-- section -->
-    <section>
+    <section class='projects'>
 
       <h1><?php the_title(); ?></h1>
 
-    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+      <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
       <!-- article -->
-      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <article id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
 
         <?php 
 
@@ -17,18 +17,24 @@
 
           $projects = get_posts( 'post_type=project' );
 
-          if( $projects ) :?>
-              <ul>
+          if ( $projects ): ?>
+            <ul class='projects-list'>
               <?php foreach( $projects as $project) : 
                 $post = $project;
-                setup_postdata( $post );?>
-                  <li><a href="<?php the_permalink(); ?>" title="Go to <?php the_title(); ?>"> <?php the_title(); ?></a></li>
+                setup_postdata( $post );
+                $featured_image = get_field('project_featured_image');
+                $description = get_field('project_description');
+              ?>
+                  <li>
+                    <h2>
+                      <a href="<?php the_permalink(); ?>" title="Go to <?php the_title(); ?>"> <?php the_title(); ?></a>
+                    </h2>
+                    <img class='featured-image' src="<?php echo $featured_image['url']; ?>" />
+                    <span><?php the_field('project_description'); ?></span>
+                  </li>
               <?php endforeach; ?>
-              </ul>
+            </ul>
           <?php endif; ?>
-
-
-        
 
         <br class="clear">
 
